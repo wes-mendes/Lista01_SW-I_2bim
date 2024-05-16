de596064ba9e4b4e552cf75d6a181eb39014c694
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-05-16 20:50:26 -0300

    Add files via upload

---
+++ exercicio9/exercicio9/Livro.cs
@@ -0,0 +1,46 @@
+﻿// Definição da classe Livro
+public class Livro
+{
+    // Atributos privados
+    private string _titulo;
+    private string _autor;
+
+    // Construtor para inicializar os atributos
+    public Livro(string titulo, string autor)
+    {
+        // Chamada do método para validar os dados
+        ValidarDados(titulo, autor);
+
+        // Atribuição dos valores aos atributos privados
+        _titulo = titulo;
+        _autor = autor;
+    }
+
+    // Propriedades Titulo e Autor
+    public string Titulo
+    {
+        get { return _titulo; }
+        set { _titulo = value; }
+    }
+
+    public string Autor
+    {
+        get { return _autor; }
+        set { _autor = value; }
+    }
+
+    // Método privado para validar os dados
+    private void ValidarDados(string titulo, string nome)
+    {
+        // Verifica se o título é vazio ou nulo
+        if (string.IsNullOrWhiteSpace(titulo))
+        {
+            throw new ArgumentException("o título do livro não pode ser vazio ou nulo!");
+        }
+        // Verifica se o nome do autor é vazio ou nulo
+        else if (string.IsNullOrWhiteSpace(nome))
+        {
+            throw new ArgumentException("o nome do autor não pode ser vazio ou nulo!");
+        }
+    }
+}
---
+++ exercicio9/exercicio9/Program.cs
@@ -0,0 +1,33 @@
+﻿// Classe interna para iniciar o programa
+internal class Program
+{
+    // Método principal
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("******** Livro ********");
+
+        // Solicita informações do livro ao usuário
+        Console.WriteLine("Digite os dados do Livro: ");
+        Console.Write("Título: ");
+        string tituloDoLivro = Console.ReadLine();
+        Console.Write("Autor: ");
+        string nomeDoAutor = Console.ReadLine();
+
+        Console.WriteLine("\n-------- Resultado --------");
+
+        try
+        {
+            // Cria instância do livro com os dados informados
+            Livro livro = new Livro(tituloDoLivro, nomeDoAutor);
+            Console.WriteLine($"Título: {livro.Titulo}");
+            Console.WriteLine($"Autor: {livro.Autor}");
+        }
+        // Captura exceções do tipo ArgumentException
+        catch (ArgumentException exception)
+        {
+            Console.WriteLine($"Erro: {exception.Message}");
+        }
+
+        Console.ReadKey();
+    }
+}
