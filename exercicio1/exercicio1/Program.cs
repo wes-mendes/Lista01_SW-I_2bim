cc7ee93bda999f60dbe219f87466f5afa7719d05
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-05-16 20:00:23 -0300

    Add files via upload

---
+++ exercicio1/exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Criando uma instância da classe Produto
+        Produto produto = new Produto("Caneta", 2.50m, 100);
+
+        // Exibindo as informações do produto
+        produto.ExibirInformacoes();
+
+        // Adicionando itens ao estoque
+        produto.AdicionarEstoque(50);
+        produto.ExibirInformacoes();
+
+        // Removendo itens do estoque
+        produto.RemoverEstoque(30);
+        produto.ExibirInformacoes();
+
+        // Tentando remover mais itens do que o disponível
+        produto.RemoverEstoque(150);
+        produto.ExibirInformacoes();
+    }
+}
