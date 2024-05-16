fb37920cb61b7cdc1993db0c066ce60b9236a2ad
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-05-16 20:53:57 -0300

    Add files via upload

---
+++ exercicio10/exercicio10/Animal.cs
@@ -0,0 +1,41 @@
+﻿// Definição da classe Animal
+public class Animal
+{
+    // Atributos privados
+    private string _nome;
+    private string _especie;
+    private int _idade;
+
+    // Construtor para inicializar os atributos
+    public Animal(string nome, string especie, int idade)
+    {
+        _nome = nome;
+        _especie = especie;
+        _idade = idade;
+    }
+
+    // Propriedades Nome, Especie e Idade
+    public string Nome
+    {
+        get { return _nome; }
+        set { _nome = value; }
+    }
+
+    public string Especie
+    {
+        get { return _especie; }
+        set { _especie = value; }
+    }
+
+    public int Idade
+    {
+        get { return _idade; }
+        set { _idade = value; }
+    }
+
+    // Método para emitir som do animal
+    public void EmitirSom(string somDoAnimal)
+    {
+        Console.WriteLine(somDoAnimal);
+    }
+}
---
+++ exercicio10/exercicio10/Program.cs
@@ -0,0 +1,32 @@
+﻿// Classe interna para iniciar o programa
+internal class Program
+{
+    // Método principal
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***** Animal *****");
+
+        // Solicita informações do animal ao usuário
+        Console.WriteLine("\nDigite as informações do Animal: ");
+        Console.Write("Nome: ");
+        string nomeDoAnimal = Console.ReadLine();
+        Console.Write("Espécie: ");
+        string especieDoAnimal = Console.ReadLine();
+        Console.Write("Idade: ");
+        int idadeDoAnimal = int.Parse(Console.ReadLine());
+        Console.Write("Som: ");
+        string somDoAnimal = Console.ReadLine();
+
+        // Cria instância do animal com os dados informados
+        Animal animal = new Animal(nomeDoAnimal, especieDoAnimal, idadeDoAnimal);
+
+        Console.WriteLine("\n----- Resultado -----");
+        Console.WriteLine($"Nome: {animal.Nome}");
+        Console.WriteLine($"Espécie: {animal.Especie}");
+        Console.WriteLine($"Idade: {animal.Idade} anos");
+        Console.Write($"O animal emite o som: ");
+        animal.EmitirSom(somDoAnimal);
+
+        Console.ReadKey();
+    }
+}
