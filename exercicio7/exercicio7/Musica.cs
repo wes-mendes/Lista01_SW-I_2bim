a09a551139a3b484bfb74ade21386ffd2fa10145
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-05-16 20:44:31 -0300

    Add files via upload

---
+++ exercicio7/exercicio7/Musica.cs
@@ -0,0 +1,13 @@
+﻿public class Musica
+{
+    public string nome;
+    public string autor;
+    public string gravadora;
+
+    public Musica(string nome, string autor, string gravadora)
+    {
+        this.nome = nome;
+        this.autor = autor;
+        this.gravadora = gravadora;
+    }
+}
---
+++ exercicio7/exercicio7/Playlist.cs
@@ -0,0 +1,30 @@
+﻿public class Playlist
+{
+    private List<Musica> listaDeMusicas = new List<Musica>();
+    public string dono;
+
+    public Playlist(string dono)
+    {
+        this.dono = dono;
+    }
+
+    public void AdicionarMusica(Musica musica)
+    {
+        listaDeMusicas.Add(musica);
+    }
+
+    public void TocarMusicaAleatoria()
+    {
+        if (listaDeMusicas.Count > 0)
+        {
+            Random random = new Random();
+            int indiceAleatorio = random.Next(listaDeMusicas.Count);
+            Musica musicaTocada = listaDeMusicas[indiceAleatorio];
+            Console.WriteLine($"Tocando a musica: {musicaTocada.nome} - {musicaTocada.autor}");
+        }
+        else
+        {
+            Console.WriteLine("A playlist está vazia. Adicione músicas antes de tocar.");
+        }
+    }
+}
